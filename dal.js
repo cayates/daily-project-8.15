98b8805b265817be55bf6fafd17f852f3c8a9af8
Chad Yates <cayates1989@gmail.com>
2017-08-15 20:45:29 -0400

    everything is working correctly up until the individual card. i cant get this to show anything EXCEPT for the available for hire string

---
+++ dal.js
@@ -0,0 +1,23 @@
+const robotsData = require('./data');
+const robotInfo = robotsData.users;
+
+function getRobots () {
+  return robotInfo
+}
+
+function getRobot (robotId) {
+    let chosenRobot = {}
+    for (let i = 0; i < robotInfo.length; i++) {
+      if (robotInfo[i].id === robotId) {
+        chosenRobot = robotInfo[i]
+      }
+      console.log(chosenRobot);
+    }
+    return chosenRobot
+  }
+
+module.exports = {
+    getRobot: getRobot,
+    getRobots: getRobots
+  }
+
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const app = express();
+const robotsData = require('./data');
+const robotInfo = robotsData.users;
+const robotDal = require('./dal')
+
+
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
+app.use(express.static('public'));
+
+app.get('/robots', function (request, response) {
+    const robots = robotDal.getRobots()
+    response.render('robots', { robots: robots })
+  })
+
+  app.get('/_robot/:id', function (request, response) {
+    const chosenRobot = robotDal.getRobot(request.params.id)
+    if (chosenRobot) {
+      response.render('robotDetail', chosenRobot)
+    } else {
+      response.send('NO ROBOTS!!!')
+    }
+  })
+
+app.set('port', 3000);
+
+app.listen(3000, function(){
+    console.log('Express started successfully.');
+})
+
+console.log(robotsData.users);
+
+// app.get('/kittehs/:id', function (request, response) {
+//   const chosenKitty = kittyDal.getKitty(request.params.id)
+//   if (chosenKitty.id) {
+//     response.render('kittyDetail', chosenKitty)
+//   } else {
+//     response.send('NO KITTEHS!!!')
+//   }
+// })
